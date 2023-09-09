c88aff284c85b9608906aabc82d20d92529eb82c
Taniya Saini <taniyasaini6871@gmail.com>
2023-09-09 12:19:37 +0530

    first commit of online-jewellery project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bluecode': '#6497ce',
+        'greycode': '#5b5b5b',
+        'lightgrey': '#a9a9a9',
+      }
+    },
+  },
+  plugins: [],
+}
+
