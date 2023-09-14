427f50db5687b3c4c6fc85cf1b9a1bdb363a79da
Taniya Saini <taniyasaini6871@gmail.com>
2023-09-14 23:19:17 +0530

    final commit of jewellery page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        'bluecode': '#6497ce',
-        'greycode': '#5b5b5b',
-        'lightgrey': '#a9a9a9',
-      }
+      
     },
   },
   plugins: [],
